c1e432cb005ed5be09e74a6d6efbfd6643c808c9
Deepak Chandan <deepak.chandan.science@gmail.com>
2015-11-28 16:35:05 -0500

    Changes to the parsing of the configuration file by the PySCRIPConfig class. This conforms to the new way configuration data is stored in the YAML config file.

--- source/config.py
+++ source/config.py
@@ -1,3 +1,10 @@
+"""
+This file provides a class for easy access to mapping files that have been
+configured on this system. To do this the class relies on configuration
+data stored as a YAML file in the user's home directory.
+
+"""
+
 import os.path as osp
 import yaml
 
@@ -5,6 +12,8 @@ class YAMLobj(dict):
     """
     Maps a YAML parsed nested dictionary to python attributes.
     This solution is a variation on http://stackoverflow.com/a/32107024/805357
+
+    We need this class to derive the PySCRIPConfig class below. 
     """
     def __init__(self, args):
         super(YAMLobj, self).__init__(args)
@@ -17,8 +26,7 @@ class YAMLobj(dict):
 
 
     def __getattr__(self, attr):
-        # return self.get(attr)
-        return getattr(self, attr)
+        return self.get(attr)
 
     def __setattr__(self, key, value):
         self.__setitem__(key, value)
@@ -38,9 +46,16 @@ class YAMLobj(dict):
 class SCRIPConfigError(Exception):
     pass
 
+
+
 class PySCRIPConfig(YAMLobj):
     defaultconfigfile = osp.join(osp.expanduser("~"), ".pyscrip.yaml")
     def __init__(self, configfile=None):
+        """
+        ARGUMENTS
+            configfile - name of config file to use. If not provided a default
+                         config file will be queried
+        """
         if configfile is None:
             if osp.isfile(PySCRIPConfig.defaultconfigfile):
                 ff = open(PySCRIPConfig.defaultconfigfile, 'r')
@@ -52,38 +67,73 @@ class PySCRIPConfig(YAMLobj):
         super(PySCRIPConfig, self).__init__( yaml.load(ff, Loader=yaml.Loader) )
         ff.close()
 
+    
+    def _mapsListForCaseAndType(self, case, maptype):
+        """
+        ARGUMENTS
+            case    - name of the case  
+            maptype - name of the map type (e.g. conservative)
+        RETURNS
+            list of maps (a map is information about going from grid A to grid B) 
+            configured for the specific case and the specific type
+        """
+        if case in self.casesAvailable():
+            if maptype in self.mapsAvailable(case):
+                return getattr(getattr(self.map, case), maptype)
+        raise SCRIPConfigError("No map information found in PySCRIP config file")
 
-    def mapfile(self, gfrom, gto, maptype="conservative"):
+
+    def mapFile(self, case, maptype, gfrom, gto):
+        """
+        ARGUMENTS
+            case    - name of the case  
+            maptype - name of the map type (e.g. conservative)
+            gfrom   - grid from which to map
+            gto     - the grid to which to map
+        RETURNS
+            full pathname to the mapping file
+        """
+        for mapconf in self._mapsListForCaseAndType(case, maptype):
+            if (mapconf["from"] == gfrom) and (mapconf["to"] == gto):
+                return osp.join(self.datadir[0], mapconf["fname"])
+        raise SCRIPConfigError("No map information found in PySCRIP config file")
+
+
+    def mapName(self, case, maptype, gfrom, gto):
         """
         Constructs and returns the name of the SCRIP netcdf mapping file corresponding
         to the map type "maptype" from grid "grom" to grid "gto".
+        ARGUMENTS
+            case    - name of the case  
+            maptype - name of the map type (e.g. conservative)
+            gfrom   - grid from which to map
+            gto     - the grid to which to map
+        RETURNS
+            description of the map
         """
-        if (hasattr(self.maptype, maptype)):
-            maps = getattr(self.maptype, maptype)
-            for mapconf in maps:
-                if (mapconf["from"] == gfrom) and (mapconf["to"] == gto):
-                    return osp.join(self.datadir[0], mapconf["fname"])
-            raise SCRIPConfigError("No information for selected mapping in PySCRIP config file")
+        for mapconf in self._mapsListForCaseAndType(case, maptype):
+            if (mapconf["from"] == gfrom) and (mapconf["to"] == gto):
+                return mapconf["name"]
+        raise SCRIPConfigError("No map information found in PySCRIP config file")
+
+
+    def mapsAvailable(self, case):
+        """
+        ARGUMENTS
+            case - name of the case
+        RETURNS
+            list of the map types configured for this case
+        """
+        val = getattr(self.map, case)
+        if val is None:
+            raise SCRIPConfigError("No information for requested 'case' in PySCRIP config file")
         else:
-            raise SCRIPConfigError("No information for selected mapping in PySCRIP config file")
+            return val.keys()
 
 
-    def mapname(self, gfrom, gto, maptype="conservative"):
+    def casesAvailable(self):
         """
-        Constructs and returns the name of the SCRIP netcdf mapping file corresponding
-        to the map type "maptype" from grid "grom" to grid "gto".
+        RETURNS
+            list of the cases currently available in the configuration file
         """
-        if (hasattr(self.maptype, maptype)):
-            maps = getattr(self.maptype, maptype)
-            for mapconf in maps:
-                if (mapconf["from"] == gfrom) and (mapconf["to"] == gto):
-                    return mapconf["name"]
-            raise SCRIPConfigError("No information for selected mapping in PySCRIP config file")
-        else:
-            raise SCRIPConfigError("No information for selected mapping in PySCRIP config file")
-
-
-
-a = PySCRIPConfig()
-# print a.maptype.conservative.fdsfs
-# print hasattr(a.maptype, "dfd")
+        return self.map.keys()
